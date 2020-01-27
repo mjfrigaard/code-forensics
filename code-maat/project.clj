b15c1bdf584212613a181736bbb47a6636678b0f
Martin Frigaard <mjfrigaard@gmail.com>
2020-01-27 12:23:45 -0800

    First commit

---
+++ code-maat/project.clj
@@ -0,0 +1,6 @@
+(defproject myproject "0.5.0-SNAPSHOT"
+  :description "A project for doing things."
+  :license "Eclipse Public License 1.0"
+  :url "http://github.com/technomancy/myproject"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-tar "3.2.0"]])
---
+++ git-logs/data/README.md
@@ -0,0 +1,14 @@
+# Git log data
+
+These data sets are from the git log.
+
+***
+
+# For guidance please see:
+
+# Sharing data
+http://bit.ly/data-4-sharing
+
+
+# Data in spreadsheets
+http://bit.ly/data-in-sheets
